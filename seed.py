19ed555739f0da731c91a511ce2ebbea00037c08
sahmiin <farihasamin03@gmail.com>
2026-02-03 15:38:43 +0000

    Seeding countries.

--- seed.py
+++ seed.py
@@ -1,2 +1,49 @@
 """This script will seed the master data into the RDS."""
 
+from os import environ as ENV
+
+from psycopg2 import connect, Error
+from pycountry import countries
+from dotenv import load_dotenv
+from psycopg2.extras import execute_batch
+
+def get_db_connection():
+    load_dotenv()
+    try:
+        connection = connect(
+            user=ENV.get("DB_USERNAME"),
+            password=ENV.get("DB_PASSWORD"),
+            host=ENV.get("DB_IP"),
+            port=ENV.get("DB_PORT"),
+            database=ENV.get("DB_NAME")
+        )
+        return connection
+    except Error as e:
+        print(f"Error connecting to database: {e}.")
+        return None
+
+
+def seed_countries():
+    rows = []
+
+    for c in countries:
+        name = getattr(c, "official_name", c.name)
+        code = c.alpha_2
+        rows.append((name, code))
+
+    conn = get_db_connection()
+    try:
+        with conn:
+            with conn.cursor() as cur:
+                execute_batch(cur, 
+                            """
+                            INSERT INTO country (country_name, country_code)
+                            VALUES (%s, %s)
+                            ON CONFLICT (country_code) DO NOTHING;
+                            """, rows, page_size=100)
+        print(f"Seeded {len(rows)} countries.")
+    finally:
+        conn.close()
+
+if __name__ == "__main__":
+    seed_countries()
