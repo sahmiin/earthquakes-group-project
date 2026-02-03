0a9023e860fbcd10dd32c9a65dd43e377130b4a0
Jordan <trainee.jordan.ogunseye@sigmalabs.co.uk>
2026-02-03 13:07:59 +0000

    Add terraform files (hopefull -secrets)

---
+++ terraform/outputs.tf
@@ -0,0 +1,35 @@
+output "rds_endpoint" {
+  value       = aws_db_instance.postgres.address
+  description = "RDS hostname"
+}
+
+output "rds_port" {
+  value       = aws_db_instance.postgres.port
+  description = "RDS port"
+}
+
+output "lambda_function_name" {
+  value       = aws_lambda_function.earthquake_job.function_name
+  description = "Lambda function name"
+}
+
+output "ecs_cluster_name" {
+  value       = data.aws_ecs_cluster.main.arn
+  description = "ECS cluster name"
+}
+
+output "ecs_service_names" {
+  value = [
+    aws_ecs_service.service_1.name,
+    aws_ecs_service.service_2.name
+  ]
+  description = "ECS service names"
+}
+
+output "ecr_repository_urls" {
+  description = "ECR repository URLs"
+  value = {
+    for k, r in aws_ecr_repository.repositories :
+    k => r.repository_url
+  }
+}
