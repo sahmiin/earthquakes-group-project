28e0101ca1e3590d0819c20706d1dbe6cbb471b0
sahmiin <farihasamin03@gmail.com>
2026-02-05 11:14:50 +0000

    add conftest and first few tests.

---
+++ weekly_report/tests/conftest.py
@@ -0,0 +1,15 @@
+"""This script contains pytest fixtures to be used across all tests in this directory."""
+
+from unittest.mock import MagicMock
+
+import pytest
+import pandas as pd
+
+@pytest.fixture
+def fake_earthquake_data():
+    ...
+
+
+@pytest.fixture
+def fake_subscribers():
+    ...
