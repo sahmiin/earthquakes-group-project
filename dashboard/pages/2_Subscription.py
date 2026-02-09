4759c6c3b1383c70f8088437b5c1dd298d8ceda2
sahmiin <farihasamin03@gmail.com>
2026-02-09 15:23:46 +0000

    again 🥀

--- dashboard/pages/2_Subscription.py
+++ dashboard/pages/2_Subscription.py
@@ -31,7 +31,7 @@ def sidebar_logo(path: str, bg="#121B2F", pad="0", radius="0px"):
 
 
 logo_path = os.path.join(os.path.dirname(__file__),
-                        "assets/tremorlytics.png")
+                        "../assets/tremorlytics.png")
 sidebar_logo(str(logo_path))
 
 
